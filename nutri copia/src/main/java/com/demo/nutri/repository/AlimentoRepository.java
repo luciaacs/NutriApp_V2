f5a6432907a557cddc9490d190f9c7892542f1ea
NutriApp-G14 <129269148+NutriApp-G14@users.noreply.github.com>
2023-04-01 01:32:13 +0200

    Add files via upload

--- nutri copia/src/main/java/com/demo/nutri/repository/AlimentoRepository.java
+++ nutri copia/src/main/java/com/demo/nutri/repository/AlimentoRepository.java
@@ -2,8 +2,11 @@ package com.demo.nutri.repository;
 
 import com.demo.nutri.model.Alimento;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface AlimentoRepository extends CrudRepository<Alimento, Integer> {
     Alimento findByName(String name);
+    List<Alimento> findByNombreUsuario(String nombreUsuario);
 }
