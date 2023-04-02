27695ddf9f55cae8dd2db7416a1da5ac1dbd8c49
NutriApp-G14 <129269148+NutriApp-G14@users.noreply.github.com>
2023-04-02 23:43:39 +0200

    Add files via upload

--- lib/model/Usuario.dart
+++ lib/model/Usuario.dart
@@ -1,13 +1,18 @@
 class Usuario {
   final String nombre;
   final String nombreUsuario;
+  final String password;
 
-  Usuario({required this.nombre, required this.nombreUsuario});
+
+  Usuario({required this.nombre, required this.nombreUsuario, required this.password});
 
   factory Usuario.fromJson(Map<String, dynamic> json) {
     return Usuario(
       nombre: json['nombre'],
       nombreUsuario: json['nombreUsuario'],
+      password: json['password'],
+
+
     );
   }
 }
