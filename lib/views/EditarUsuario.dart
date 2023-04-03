f23549bac3b5aa7632df859d355032159293bbd1
luciaacs <106026951+luciaacs@users.noreply.github.com>
2023-04-03 10:38:46 +0200

    Update EditarUsuario.dart

--- lib/views/EditarUsuario.dart
+++ lib/views/EditarUsuario.dart
@@ -25,6 +25,9 @@ class _EditarUsuarioPageState extends State<EditarUsuarioPage> {
   DataBaseHelper dataBaseHelper = DataBaseHelper();
   final TextEditingController nombreController = TextEditingController();
   final TextEditingController nombreUsuarioController = TextEditingController();
+  final TextEditingController EdadController = TextEditingController();
+  final TextEditingController PesoController = TextEditingController();
+  final TextEditingController AlturaController = TextEditingController();
 
   @override
   void initState() {
@@ -45,12 +48,40 @@ class _EditarUsuarioPageState extends State<EditarUsuarioPage> {
 
   @override
   Widget build(BuildContext context) {
+    var _generoSeleccionado;
+    var _nivelActividadSeleccionado;
+    List<Map<String, dynamic>> _alergias = [
+      {'titulo': 'Polen', 'valor': false},
+      {'titulo': 'Lactosa', 'valor': false},
+      {'titulo': 'Mariscos', 'valor': false},
+      {'titulo': 'Nueces', 'valor': false},
+    ];
+    List<CheckboxListTile> _crearAlergias() {
+      List<CheckboxListTile> alergias = [];
+
+      for (int i = 0; i < _alergias.length; i++) {
+        alergias.add(
+          CheckboxListTile(
+            title: Text(_alergias[i]['titulo']),
+            value: _alergias[i]['valor'],
+            onChanged: (bool? newValue) {
+              setState(() {
+                _alergias[i]['valor'] = newValue ?? false;
+              });
+            },
+          ),
+        );
+      }
+
+      return alergias;
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Editar Usuario'),
       ),
       body: Container(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(10.0),
         child: FutureBuilder<Usuario>(
           future: _futureUsuario,
           builder: (context, snapshot) {
@@ -71,7 +102,7 @@ class _EditarUsuarioPageState extends State<EditarUsuarioPage> {
                   Text(
                     'Nombre de usuario',
                     style: TextStyle(
-                      fontSize: 16.0,
+                      fontSize: 10.0,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
@@ -79,11 +110,11 @@ class _EditarUsuarioPageState extends State<EditarUsuarioPage> {
                   Text(
                     usuario.nombreUsuario,
                     style: TextStyle(
-                      fontSize: 20.0,
+                      fontSize: 10.0,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  const SizedBox(height: 16.0),
+                  const SizedBox(height: 10.0),
                   Text(
                     'Nombre',
                     style: TextStyle(
@@ -99,6 +130,99 @@ class _EditarUsuarioPageState extends State<EditarUsuarioPage> {
                     ),
                   ),
                   const SizedBox(height: 16.0),
+                  TextField(
+                    controller: EdadController,
+                    decoration: InputDecoration(
+                      labelText: 'Edad',
+                      icon: Icon(Icons.cake),
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  TextField(
+                    controller: AlturaController,
+                    decoration: InputDecoration(
+                      labelText: 'Altura',
+                      hintText: 'Altura en cm',
+                      icon: Icon(Icons.height),
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  TextField(
+                    controller: PesoController,
+                    decoration: InputDecoration(
+                      labelText: 'Peso',
+                      hintText: 'Peso en kg',
+                      icon: Icon(Icons.fitness_center),
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  DropdownButtonFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Género',
+                      icon: Icon(Icons.wc),
+                    ),
+                    value: _generoSeleccionado,
+                    onChanged: (value) {
+                      setState(() {
+                        _generoSeleccionado = value;
+                      });
+                    },
+                    items: [
+                      DropdownMenuItem(
+                        value: 'hombre',
+                        child: Text('Hombre'),
+                      ),
+                      DropdownMenuItem(
+                        value: 'mujer',
+                        child: Text('Mujer'),
+                      ),
+                    ],
+                  ),
+                  DropdownButtonFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Nivel de actividad física',
+                      icon: Icon(Icons.directions_run),
+                    ),
+                    value: _nivelActividadSeleccionado,
+                    onChanged: (value) {
+                      setState(() {
+                        _nivelActividadSeleccionado = value;
+                      });
+                    },
+                    items: [
+                      DropdownMenuItem(
+                        value: 'nada activo',
+                        child: Text('Nada activo'),
+                      ),
+                      DropdownMenuItem(
+                        value: 'poco activo',
+                        child: Text('Poco activo'),
+                      ),
+                      DropdownMenuItem(
+                        value: 'activo',
+                        child: Text('Activo'),
+                      ),
+                      DropdownMenuItem(
+                        value: 'muy activo',
+                        child: Text('Muy activo'),
+                      ),
+                    ],
+                  ),
+                  // CheckboxListTile para seleccionar alergias
+                  Text(
+                    'Alergias:',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 10.0,
+                    ),
+                  ),
+
+                  SizedBox(height: 8.0),
+
+                  Column(
+                    children: _crearAlergias(),
+                  ),
+
                   ElevatedButton(
                     onPressed: () {
                       dataBaseHelper.updateUsuario(
